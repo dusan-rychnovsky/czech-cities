c7c0471ca3ff19a26aa354005c89b18b1cbb4a16
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-05-08 12:31:25 +0200

    update map

--- filter.rb
+++ filter.rb
@@ -1,17 +1,36 @@
-File.open('db.csv').each do |line|
+basic_info = File.readlines('db-wiki.csv').each_with_object({}) do |line, acc|
   next if line.strip!.empty?
-  id, city, population, area, district, lat, lon, distance_from_hranice, distance_from_caslav = line.split(',')
+  id, city, population, _, _ = line.split(',')
+  next if id == "id"  
+  acc[id] = { :city => city, :population => population.to_i }
+end
+
+File.readlines('db-gps.csv').each do |line|
+  next if line.strip!.empty?
+  id, lat, lon = line.split(',')
   next if id == "id"
+  basic_info[id][:lat] = lat
+  basic_info[id][:lon] = lon
+end
 
-  population = population.to_i
-  distance_from_caslav = distance_from_caslav.to_i
-  distance_from_hranice = distance_from_hranice.to_i
+def load_distances(filename)
+  File.readlines(filename).each_with_object({}) do |line, acc|
+    next if line.strip!.empty?
+    id, _, time = line.split(',')
+    next if id == "id"  
+    acc[id] = { :time => time.to_f }
+  end
+end
 
-  if population >= 10_000
-    if distance_from_caslav <= 100 && distance_from_hranice <= 155
-      puts "\"#{city}\": [\"red\",#{lat},#{lon}],"
+distances_hranice = load_distances 'car-distances-hranice.csv'
+distances_caslav = load_distances 'car-distances-caslav.csv'
+
+basic_info.each do |id, value|
+  if value[:population] >= 10_000
+    if distances_hranice[id][:time] <= 2.5 && distances_caslav[id][:time] <= 2
+      puts "\"#{value[:city]}\": [\"red\",#{value[:lat]},#{value[:lon]}],"
     else
-      puts "\"#{city}\": [\"blue\",#{lat},#{lon}],"
+      puts "\"#{value[:city]}\": [\"blue\",#{value[:lat]},#{value[:lon]}],"
     end
   end
 end
